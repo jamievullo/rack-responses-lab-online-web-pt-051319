591ee40397e6c4a0867ad15f5ddb44b2c7fec987
Jamie Vullo <jvullo@protonmail.com>
2019-07-29 14:26:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,11 @@ class Application
     resp = Rack::Response.new 
     
     if Time.now < 12:00
-    resp.write "Good Morning!\n"
-  else
-    resp.write "Good Afternoon!\n"
-  end
-    resp.finish
+      resp.write "Good Morning!\n"
+    else
+      resp.write "Good Afternoon!\n"
+    end
+      resp.finish
   end
   
 end
